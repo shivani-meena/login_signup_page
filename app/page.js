165f7c6c66e48ef9b5d1aa82db635200551a6180
Shivani Meena <meenashivani133@gmail.com>
2024-08-05 10:39:27 +0530

    Created folder

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
